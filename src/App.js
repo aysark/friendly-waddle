3f9875ea9ef72f777ea77b1bcaa253012f5c0654
PJ Briggs <briggs.pj@gmail.com>
2017-02-11 12:50:12 -0800

    faq and about page

--- src/App.js
+++ src/App.js
@@ -15,12 +15,13 @@ class App extends Component {
       placeholder: '',
       isFetching: false,
       response: '',
+      page: 'home',
       scream:''
     }
   } 
   componentWillMount(){
     let currentIndex = 0
-    this.setState({placeholder: placeholders[currentIndex]});
+    this.changeText.call(this, placeholders[currentIndex]);
     let intervalID = setInterval(()=>{
         ++currentIndex;
         if (currentIndex >= placeholders.length) {
@@ -67,58 +68,96 @@ class App extends Component {
 
 
   render() {
+    let {placeholder, scream, page } = this.state;
+    let content;
 
+    let Home =(
+      <div className="ui text container" >
+        <h1 className="ui inverted header">
+          I Want Someone To Scream...
+        </h1>
+        <input className="myBox" onChange={(e)=> this.changeScream(e)}placeholder={this.state.placeholder} value={this.state.scream}/>
+        <br/>
+        <div className="ui huge primary button myButton" onClick={(e)=>this.submitForm()}>Go <i className="right arrow icon"></i></div>
+      </div>
+    )
+
+    let FAQ =(
+      <div className="ui text container" >
+        <h1 className="ui inverted header">
+         FAQ
+        </h1>
+        <h2> What is Echo.cash? </h2>
+        <p> Really Amazing</p>
+      </div>
+    )
+
+    let About =(
+      <div className="ui text container" >
+        <h1 className="ui inverted header">
+         About Us
+        </h1>
+        <p> 
+          We are the coolest site. We Started at Startup Weekend Orange County
+        </p>
+      </div>
+    )
+
+    switch(page){
+      case 'home':
+        content = Home
+        break;
+      case 'faq':
+        content = FAQ
+        break;
+      case 'about':
+        content = About
+        break;
+    }
     return (
-      <div className="App" style={{'background': 'linear-gradient(rgba(0, 40, 0, .6), rgba(0, 0, 0, .3)), url(http://cdn2.collective-evolution.com/assets/uploads/2016/08/yelling.jpg)', 'backgroundSize': 'cover', 'minHeight': '100%' }}>
-        <div className="ui inverted vertical masthead center aligned segment" style={{'background': 'linear-gradient(rgba(0, 40, 0, .6), rgba(0, 0, 0, .3)), url(http://cdn2.collective-evolution.com/assets/uploads/2016/08/yelling.jpg)', 'backgroundSize': 'cover'}}>
-
-            <div className="ui container">
-              <div className="ui large secondary inverted pointing menu">
-                <a className="toc item">
-                  <i className="sidebar icon"></i>
-                </a>
-                <a className="active item">Home</a>
-                <a className="item">FAQ</a>
-                <a className="item">About Us</a>
-                <a className="item">Careers</a>
-                <div className="right item">
-                  <a className="ui inverted button">Log in</a>
-                  <a className="ui inverted button">Sign Up</a>
+      <div className="App" style={{'background': 'url(http://cdn2.collective-evolution.com/assets/uploads/2016/08/yelling.jpg)', 'backgroundSize': 'cover', 'minHeight': '100vh' }}>
+        <div className="App" style={{'background': 'linear-gradient(rgba(0,0,0,.5), rgba(0,100,0,.7))', 'backgroundSize': 'cover', 'minHeight': '100vh' }}>
+          <div className="ui inverted vertical masthead center aligned segment" style={{ 'background': 'transparent'}}>
+  
+              <div className="ui container">
+                <div className="ui large secondary inverted pointing menu">
+                  <a className="toc item">
+                    <i className="sidebar icon"></i>
+                  </a>
+                  <a onClick={ (e)=> this.setState({page:'home'}) } className={ page==='home' ? 'active item': 'item' }>Home</a>
+                  <a onClick={ (e)=> this.setState({page:'faq'}) } className={ page==='faq' ? 'active item': 'item' }>FAQ</a>
+                  <a onClick={ (e)=> this.setState({page:'about'}) } className={ page==='about' ? 'active item': 'item' }>About Us</a>
+                  <div className="right item">
+                    <a className="ui inverted button">Log in</a>
+                    <a className="ui inverted button">Sign Up</a>
+                  </div>
                 </div>
               </div>
-            </div>
-
-            <div className="ui text container">
-              <h1 className="ui inverted header">
-                I Want Someone To Scream...
-              </h1>
-              <input className="myBox" onChange={(e)=> this.changeScream(e)}placeholder={this.state.placeholder} value={this.state.scream}/>
-              <br/>
-              <div className="ui huge primary button myButton" onClick={(e)=>this.submitForm()}>Go <i className="right arrow icon"></i></div>
-            </div>
-
+              {content}
+  
           </div>
-        {/*<Container text>
-            <div className="logoContainer">
-              <Segment inverted circular color='yellow' className='logo'><Header as='h1'>ECHO</Header></Segment>
-            </div>
-
-            <div className="mainContent">
-              <Segment padded='very' attached>
-                <p>Text this number</p>
-                  <a href="tel:+18058641118">
-                  <Button basic size='massive' color='blue'>
-                    <Icon name='phone' /> +1 (805) 864-1118
-                  </Button>
-                </a>
-              </Segment>
-              <Message warning attached='bottom'>
-                <Icon name='warning' />
-                Only for the brave
-              </Message>
-            </div>
-
-        </Container>*/}
+          {/*<Container text>
+              <div className="logoContainer">
+                <Segment inverted circular color='yellow' className='logo'><Header as='h1'>ECHO</Header></Segment>
+              </div>
+  
+              <div className="mainContent">
+                <Segment padded='very' attached>
+                  <p>Text this number</p>
+                    <a href="tel:+18058641118">
+                    <Button basic size='massive' color='blue'>
+                      <Icon name='phone' /> +1 (805) 864-1118
+                    </Button>
+                  </a>
+                </Segment>
+                <Message warning attached='bottom'>
+                  <Icon name='warning' />
+                  Only for the brave
+                </Message>
+              </div>
+  
+          </Container>*/}
+        </div>
       </div>
     );
   }
